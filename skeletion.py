45524bbd4bf25feab08dc2da524d4644a958e2d0
JooJaeseung <jjs7856@kookmin.ac.kr>
2025-12-08 15:32:05 +0900

    Inital mainTask logic

--- skeletion.py
+++ skeletion.py
@@ -2,6 +2,11 @@ import threading
 from time import sleep
 
 # 대충 핀 설정
+zoneID = {'IDLE' : 0, 'CHILD' : 1, 'HIGHACCIDENT' : 2, 'SPEEDBUMP' : 3}
+current_zone = 0
+Child_pwm = 30 # 어린이 보호구역 목표 속도
+HighAccident_pwm = 50 # 사고다발지역 목표 속도
+SpeedBump_pwm = 20 # 과속 방지턱 목표 속도
 
 stop_event = threading.Event()
 
@@ -16,10 +21,25 @@ def getObjInfoTask(stop_flag: threading.Event, 추가로 인자들):
         sleep(0.1)
 
 # 이게 메인 로직
-def mainTask(stop_flag: threading.Event, 추가로 인자들):
+def mainTask(stop_flag: threading.Event, ZoneInfo, isObjDetected, isObjInRoad):
     while not stop_flag.is_set():
-        # 여기에 짜면 돼
-        sleep(0.1)
+         if isObjDetected == 1 and isObjInRoad == 1:
+            current_pwm = 0
+         elif ZoneInfo == zoneID['IDLE']:
+            continue
+         elif ZoneInfo == zoneID['CHILD']:
+            current_pwm = Child_pwm
+         elif ZoneInfo == zoneID['HIGHACCIDENT']:
+            current_pwm = HighAccident_pwm
+         elif ZoneInfo == zoneID['SPEEDBUMP']:
+            while SpeedBump_pwm > current_pwm:
+               current_pwm -= 2
+            if current_pwm < SpeedBump_pwm:
+               current_pwm == SpeedBump_pwm    
+               
+               sleep(0.1)
+    
+         sleep(0.1)
 
 def getCurZone(인자):
     # 여기 짜면 돼
