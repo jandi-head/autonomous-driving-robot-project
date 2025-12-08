a98402625b356fb033c097c17ca0229886e55f2c
Hyeongyeom <brigade98@naver.com>
2025-12-08 15:41:53 +0900

    스켈레톤 코드 updated(현겸)

--- skeletion.py
+++ skeletion.py
@@ -1,3 +1,9 @@
+'''
+자료형 bool
+isObjDetected
+isObjInRoad
+'''
+
 import threading
 from time import sleep
 
@@ -5,53 +11,57 @@ from time import sleep
 
 stop_event = threading.Event()
 
-def getCurZoneTask(stop_flag: threading.Event, 추가로 인자들):
+def getCurZoneTask(stop_flag: threading.Event, image):
     while not stop_flag.is_set():
-        zoneInfo = getCurZone(인자)
+        zoneInfo = getCurZone(image)
         sleep(0.1)
 
-def getObjInfoTask(stop_flag: threading.Event, 추가로 인자들):
+def getObjInfoTask(stop_flag: threading.Event, distUS, image):
     while not stop_flag.is_set():
-        isObjDetected, isObjInRoad = getObjInfo(인자)
+        isObjDetected, isObjInRoad = getObjInfo(distUS, image)
         sleep(0.1)
 
 # 이게 메인 로직
-def mainTask(stop_flag: threading.Event, 추가로 인자들):
+def mainTask(stop_flag: threading.Event,ZoneInfo, isObjDetected, isObjInRoad):
     while not stop_flag.is_set():
         # 여기에 짜면 돼
         sleep(0.1)
-
-def getCurZone(인자):
+    limVel = 20.0 # [-100 100]   
+    return limVel
+def getCurZone(image):
     # 여기 짜면 돼
     zoneInfo = 0
     return zoneInfo
 
-def getObjDetected(인자):
+def getObjDetected(image):
     # 여기 짜면 돼
-    isObjDetected = 0
+    isObjDetected = True
     return isObjDetected
 
-def getObjInRoad(인자):
+def getObjInRoad(image):
     # 여기 짜면 돼
-    isObjInRoad = 0
+    isObjInRoad = True
     return isObjInRoad
 
-def getObjInfo(인자):
+def getObjInfo(image):
     # 여기 짜면 돼
     
     # 센서퓨전
-    isObjDetected = getObjDetected(인자)
-    isObjInRoad = getObjInRoad(인자)
+    isObjDetected = getObjDetected(image)
+    isObjInRoad = getObjInRoad(image)
     return isObjDetected, isObjInRoad
 
 
 if __name__ == "__main__":
     
-    curZoneTaskThread = threading.Thread(인자들)
+    curZoneTaskThread = threading.Thread(target=getCurZoneTask, args=(stop_event,),
+ name="curZoneTaskThread")
     
-    objInfoTaskThread = threading.Thread(인자들)
-    
-    mainTaskThread = threading.Thread(인자들)
+    objInfoTaskThread = threading.Thread(target=getObjInfoTask, args=(stop_event,),
+ name="ObjInfoTaskThread")
+
+    mainTaskThread = threading.Thread(target=mainTask, args=(stop_event,),
+ name="mainTaskThread")
     
     curZoneTaskThread.start()
     objInfoTaskThread.start()
